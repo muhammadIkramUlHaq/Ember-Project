192a5be46978d078bca24dbe3f618e9029e6fd7a
farooquemustafa <farooq_unar@outlook.com>
2021-10-03 17:52:03 +0200

    styling almost done

--- app/routes/application.js
+++ app/routes/application.js
@@ -9,13 +9,15 @@ export default Ember.Route.extend({
     let projects = data.projects;
 
     return projects.map((project) => {
-      let { name, status, id, rate, summary, illustration } = project;
+      let { name, status, id, rate, summary, illustration, amount, grade, onlineDate, expirationDate, openingDate, loanDuration  } = project;
       //let summary_en = summary.map((s) => s.en.value);
       let summary_en = summary.en[0].value;
 
       let image = 'https://cdn.october.eu/' + illustration.url;
+      let projectDurationLeft 
+      let onlineStatus = status
 
-      return { name, status, id, rate, summary_en, image };
+      return { name, status, id, rate, summary_en, image, grade, amount, expirationDate, loanDuration  };
     });
   },
 });
