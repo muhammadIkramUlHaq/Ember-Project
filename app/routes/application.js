f0ce15a3b3bc2546d3050a51b6e5ef0000bfa087
muhammadIkramUlHaq <ikramulhaq1992@gmail.com>
2021-10-04 02:32:38 +0200

    More style chanegs

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,6 @@
 /* eslint-disable ember/new-module-imports */
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Route.extend({
   async model() {
@@ -8,7 +9,13 @@ export default Ember.Route.extend({
 
     let projects = data.projects;
 
-    return projects.map((project) => {
+    let onlineProjectCount = 0;
+
+    let parsedProjects = projects.map((project) => {
+      if (project.status == 'online') {
+        onlineProjectCount++;
+      }
+
       let {
         name,
         status,
@@ -19,16 +26,14 @@ export default Ember.Route.extend({
         amount,
         grade,
         onlineDate,
-        expirationDate,
-        openingDate,
         loanDuration,
       } = project;
-      //let summary_en = summary.map((s) => s.en.value);
+
       let summary_en = summary.en[0].value;
 
       let image = 'https://cdn.october.eu/' + illustration.url;
-      let projectDurationLeft;
-      let onlineStatus = status;
+
+      let projectStartedDuration = getProjectStartedDuration(onlineDate);
 
       return {
         name,
@@ -39,9 +44,34 @@ export default Ember.Route.extend({
         image,
         grade,
         amount,
-        expirationDate,
+        projectStartedDuration,
         loanDuration,
       };
     });
+
+    return { parsedProjects, onlineProjectCount };
   },
 });
+
+function getProjectStartedDuration(projectOnlineDate) {
+  let durationFormats = ['day', 'days', 'month', 'months', 'year', 'years'];
+  let now = moment();
+  let formattedOnlineDate = moment(projectOnlineDate);
+  let projectStartedDuration = now.diff(formattedOnlineDate, 'days');
+  let postFixValue =
+    projectStartedDuration > 1 ? durationFormats[1] : durationFormats[0];
+
+  if (projectStartedDuration > 30) {
+    projectStartedDuration = now.diff(formattedOnlineDate, 'months');
+    postFixValue =
+      projectStartedDuration > 1 ? durationFormats[3] : durationFormats[2];
+
+    if (projectStartedDuration > 12) {
+      projectStartedDuration = now.diff(formattedOnlineDate, 'years');
+      postFixValue =
+        projectStartedDuration > 1 ? durationFormats[5] : durationFormats[4];
+    }
+  }
+
+  return projectStartedDuration + ' ' + postFixValue;
+}
